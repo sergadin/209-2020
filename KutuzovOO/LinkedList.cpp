cb3a40b6a94af64f4be402a3e3c5698d2265c0cc
KKKutuzov <62026495+KKKutuzov@users.noreply.github.com>
2020-04-06 11:22:30 +0300

    Added a full implementation of a hash table for storing database records

---
+++ KutuzovOO/LinkedList.cpp
@@ -0,0 +1,122 @@
+#include "LinkedList.h"
+#include <iostream>
+
+LinkedList::LinkedList() //default constructor
+{
+	front=NULL;
+}
+
+LinkedList::LinkedList(const LinkedList& ls) //copy constructor
+{
+	deepCopy(ls);
+}
+
+LinkedList::~LinkedList() //destructor
+{
+	deleteList();
+}
+
+LinkedList& LinkedList::operator=(const LinkedList& ls) //assignment operator
+{
+	if(this!=&ls)
+	{
+		deleteList();
+		deepCopy(ls);
+	}
+	return *this;
+}
+
+bool LinkedList::insert(const line& s)
+{
+	if(search(s))
+		return false;
+	front=new Node(s,front);
+	return true;
+}
+
+bool LinkedList::remove(const line& s)
+{
+	Node* temp=front;
+	if(temp==NULL) //list is empty
+		return false;
+	if(temp->data==s) //s is first string in list
+	{
+		front=temp->next;
+		delete temp;
+		return true;
+	}
+	else
+	{
+		while(temp->next!=NULL){
+			if(temp->next->data==s)
+			{
+				Node* deletedNode=temp->next;
+				temp->next=temp->next->next;
+				delete deletedNode;
+				return true;
+			}
+			temp=temp->next;
+		}
+		return false;
+	}
+}
+
+void  LinkedList::printList() const
+{
+    Node* temp=front;
+    while(temp!=NULL)
+    {
+        std::cout << temp->data.name << " ";
+        temp = temp->next;
+    }
+}
+
+bool LinkedList::search(const line& s) const
+{
+	Node* temp=front;
+	while(temp!=NULL) //Traverse list
+	{
+		if(temp->data==s)
+			return true;
+		temp = temp->next;
+	}
+	return false;
+}
+
+std::vector<line> LinkedList::get() const
+{
+	std::vector<line> str_vec;
+	for(Node* temp=front;temp!=NULL;temp=temp->next)
+		str_vec.push_back(temp->data);
+	return str_vec;
+}
+
+void LinkedList::deepCopy(const LinkedList& ls)
+{
+	front=NULL;
+	if(ls.front!=NULL) //Only copy if ls is non-empty
+	{
+		Node* original=ls.front;
+		Node* copy;
+		copy=new Node(original->data, NULL);
+		front=copy;
+		original=original->next;
+		while(original!=NULL) //Traverse the original copying each node
+		{
+			copy->next=new Node(original->data, NULL);
+			copy=copy->next;
+			original=original->next;
+		}
+	}
+}
+
+
+void LinkedList::deleteList()
+{
+	Node* tmp;
+	while(front!=NULL){
+		tmp=front->next;
+		delete front;
+		front=tmp;
+	}
+}
---
+++ KutuzovOO/LinkedList.h
@@ -0,0 +1,58 @@
+#ifndef LINKEDLIST_H_INCLUDED
+#define LINKEDLIST_H_INCLUDED
+
+#include <cstdlib>
+#include <vector>
+#include <string>
+
+
+struct line
+{
+  std::string name;
+  line& operator=(const line& rhs)
+  {
+      name = rhs.name;
+      return *this;
+  }
+  bool  operator==(const line &rhs)
+  {
+      return name == rhs.name;
+  }
+};
+
+class LinkedList
+{
+public:
+	LinkedList(); //default constructor
+	LinkedList(const LinkedList& ls);//copy constructor
+	~LinkedList(); //destructor
+	LinkedList& operator=(const LinkedList&); //assignment operator
+	bool insert(const line&);
+	bool remove(const line&);
+	bool search(const line&) const;
+	std::vector<line> get() const;
+  void printList() const;
+
+private:
+	class Node
+	{
+	public:
+		line data;
+		Node* next;
+		Node(line s)
+    {
+        data = s;
+        next = NULL;
+    }
+		Node(line s,Node* nd)
+    {
+        data = s;
+        next = nd;
+    }
+	};
+	Node* front;
+	void deepCopy(const LinkedList& ls);
+	void deleteList();
+};
+
+#endif
