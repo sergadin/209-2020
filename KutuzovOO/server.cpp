e0c073392bfd49d3af070b27b1cede99e887af16
KKKutuzov <62026495+KKKutuzov@users.noreply.github.com>
2020-05-18 19:36:29 +0300

    Corrected the server

--- KutuzovOO/server.cpp
+++ KutuzovOO/server.cpp
@@ -4,50 +4,76 @@
 //                                                            //
 ////////////////////////////////////////////////////////////////
 
-
 #include <iostream>
 #include "database.h"
 #include "datagenerator.h"
-
-
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
 
 // g++ server.cpp database.cpp HashTable.cpp LinkedList.cpp -o server
+
 using namespace std;
 
 int main()
 {
-    /*  makedatabase(1000,10,"items.txt","recipe.txt");
-      Database x("items.txt","recipe.txt");
-      x.DatabaseToFile("kek.txt","pek.txt");*/
-      Database x("test1.txt","test2.txt");
-      map<string,int> r;
-      r["kartoshka"] = 2;
-      r["svekla"] = 3;
-      x.AddDetail("zemlya",10);
-      x.AddRecipe("Borsh",r);
-      x.print();
-      cout << endl;
-      x.deleteDetail("zemlya");
-      x.print();
-      cout << endl;
-      x.CanMake("Borsh");
-      cout << endl;
-      x.deleteDetail("palka",5);
-      x.AddDetail("svekla",5);
-      x.print();
-      cout << endl;
-      x.CanMake("Borsh");
-      cout << endl;
-      x.AddDetail("kartoshka",2);
-      x.MakeDetail("Borsh");
-      x.print();
-
-      cout << x.dbsize() << endl;
-
-
-
-      return 0;
-
-
+    cout << "Welcome to the warehouse\n";
+    cout << "Which database would you like to upload?\n";
+    cout << "a) random generate\n";
+    cout << "b) test\n";
+    string var;
+    cin >> var;
+    Database x;
+    if(var == "test")
+    {
+          x.DatabaseFromFile("test_data.txt","test_recipe.txt");
+    }
+    else
+    {
+        makedatabase(1000,10,"items.txt","recipe.txt");
+        x.DatabaseFromFile("items.txt","recipe.txt");
+    }
+    cout << "The database has been loaded successfully, and you can start working\n";
+    while(1)
+    {
+      string zapros;
+      cin >> zapros;
+      std::transform(zapros.begin(), zapros.end(), zapros.begin(), ::tolower);
+      if(zapros == "exit")
+      {
+          cout << "Good bye!\n";
+          break;
+      }
+      if(zapros == "show")
+      {
+          x.print();
+      }
+      if(zapros == "canmake")
+      {
+          string n;
+          cin >> n;
+          if(x.CanMake(n) == 1)
+          {
+              cout << "Y\n";
+          }
+      }
+      if(zapros == "make")
+      {
+        string n;
+        cin >> n;
+        if(x.CanMake(n) == 1)
+        {
+            x.MakeDetail(n);
+        }
+      }
+      if(zapros == "adddetail")
+      {
+            string  n;
+            int q;
+            cin >> n >> q;
+            x.AddDetail(n,q);
+      }
+    }
+    return 0;
 }
