342666aa1f9f5eac5e204ee92a22aa550442cf8d
gruuuunt <grant.ant.chess@gmail.com>
2020-06-04 19:21:03 +0300

    почти всё

--- AntonianGV/server.cpp
+++ AntonianGV/server.cpp
@@ -1,12 +1,23 @@
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <io.h>
-#include <unistd.h>
+
+
+#define PORT    5555
+#define BUFLEN  1024
+
+
+int   readFromClient(int fd, char *buf);
+void  writeToClient (int fd, char *buf);
 char * show (char**, int);
 char * add (char**, int, char*, int);
 char * del (char**, int, int);
@@ -18,12 +29,12 @@ int kek (int);
 void itoa(int n, char s[]) {
      int i, sign;
  
-     if ((sign = n) < 0)  /* записываем знак */
-         n = -n;          /* делаем n положительным числом */
+     if ((sign = n) < 0) 
+         n = -n;          
      i = 0;
-     do {       /* генерируем цифры в обратном порядке */
-         s[i++] = n % 10 + '0';   /* берем следующую цифру */
-     } while ((n /= 10) > 0);     /* удаляем */
+     do {     
+         s[i++] = n % 10 + '0';   
+     } while ((n /= 10) > 0);    
      if (sign < 0)
          s[i++] = '-';
      s[i] = '\0';
@@ -139,30 +150,53 @@ char * del (char ** text, int N, int paragraph) {
 	strcpy (answer, "Done\n");
 	return answer;
 }
-int main(int argc, char *argv[])
+
+int  main (void)
 {
-    int as, ms;
-    struct sockaddr_in server;
-    char buf[1024]; 
+
+    int     i, err, opt=1;
+    int     sock, new_sock;
+    fd_set  active_set, read_set;
+    struct  sockaddr_in  addr;
+    struct  sockaddr_in  client;
+    char    buf[BUFLEN];
+    socklen_t  size;
 	char* answer;
 	int str_length;
 	char c = '0';
-	int i, j, Par_Num = 0, length; //number of paragraphs
+	int k, j, Par_Num = 0, length; 
 	char ** text;
 	int chislo;
 	FILE* in;
-    as = socket(AF_INET, SOCK_STREAM, 0 ); 
 
- 
-    server.sin_family = AF_INET; 
-    server.sin_addr.s_addr = INADDR_ANY; 
-    server.sin_port = htons(1234); 
+   
+    sock = socket (PF_INET, SOCK_STREAM, 0);
+    if ( sock<0 ) {
+        perror ("Server: cannot create socket");
+        exit (EXIT_FAILURE);
+    }
+    setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,(char*)&opt,sizeof(opt));
 
 
-    bind(as, (struct sockaddr *) &server, sizeof(server));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    err = bind(sock,(struct sockaddr*)&addr,sizeof(addr));
+    if ( err<0 ) {
+        perror ("Server: cannot bind socket");
+        exit (EXIT_FAILURE);
+    }
 
-    listen(as, 5); 
-	
+   
+    err = listen(sock,3);
+    if ( err<0 ) {
+        perror ("Server: listen queue failure");
+        exit(EXIT_FAILURE);
+    }
+
+    
+    FD_ZERO(&active_set);
+    FD_SET(sock, &active_set);
 	in = fopen ("text.txt","r");
 	while (c != EOF) {
 		c = fgetc(in);
@@ -175,93 +209,160 @@ int main(int argc, char *argv[])
 		printf ("Mem Error\n");
 		exit(1);
 	}
-	for (i = 0; i < Par_Num; i++) {
-		text[i] = (char*)malloc(1026);
+	for (k = 0; k < Par_Num; k++) {
+		text[k] = (char*)malloc(1026);
 	}
-	for (i = 0; i < Par_Num; i++) {
-		fgets (text[i], 1024, in);
-		length = strlen(text[i]);
-		if (text[i][length-1] == '\n'||text[i][length-1] == 13) text[i][length-1] = '\0';
-		if (text[i][length-2] == '\n'||text[i][length-2] == 13) text[i][length-2] = '\0';
+	for (k = 0; k < Par_Num; k++) {
+		fgets (text[k], 1024, in);
+		length = strlen(text[k]);
+		if (text[k][length-1] == '\n'||text[k][length-1] == 13) text[k][length-1] = '\0';
+		if (text[k][length-2] == '\n'||text[k][length-2] == 13) text[k][length-2] = '\0';
 	}
 	fclose(in);
-    while(1) {
-		//ms = accept( as, 0, 0 );
-        bzero( buf, sizeof(buf)); 
-		gets (buf);
-		/*if (readFromClient(ms, buf)<0){
-			exit(1);
-		}*/
-		//read(ms, buf, sizeof(buf));
-		//printf ("%s\n",buf);
-        if (strcmp(buf, "quit") == 0 ) {
-			answer = (char*)malloc(7);
-			strcpy (answer, "Quit\n");
-			puts (answer);
-			free (answer);
-			break;
-		}
-		if (strncmp(buf, "show", 4) == 0) answer = show(text, Par_Num);
-		if (strncmp(buf, "add", 3) == 0) {
-			i = 3;
-			while (buf[i] < '0'&&buf[i]>'9') i++;
-			chislo = atoi(&buf[i]);
-			i = i + kek(chislo)+1;
-			while (buf[i] == ' ') i++;
-			if (chislo < 0||chislo > Par_Num+1) {
-				answer = (char*)malloc(22);
-				strcpy (answer, "Incorrect number\n");
-			}
-			else {
-				text = (char**)realloc(text, (Par_Num+1)*sizeof(char*));
-				text[Par_Num] = (char*)malloc(1026);
-				answer = add (text, Par_Num, &buf[i], chislo);
-				Par_Num++;
-			}
-		}
-		if (strncmp(buf, "search", 6) == 0) {
-			i = 6;
-			while (buf[i] == ' ') i++;
-			chislo = atoi(&buf[i]);
-			i = i + kek(chislo) + 1;
-			while (buf[i] == ' ') i++;
-			answer = search (text, Par_Num, &buf[i], chislo);
-		}
-		if (strncmp(buf, "delete", 6) == 0) {
-			i = 6;
-			while (buf[i] < '0'&&buf[i]>'9') i++;
-			chislo = atoi(&buf[i]);
-			if (chislo < 0||chislo > Par_Num) {
-				answer = (char*)malloc(22);
-				strcpy (answer, "Incorrect number\n");
-			}
-			else {
-				answer = del (text, Par_Num, chislo);
-				free (text[Par_Num-1]);
-				text = (char**)realloc(text, (Par_Num-1)*sizeof(char*));
-				Par_Num--;
-			}
-		}
-		if (answer == NULL) {
-			answer = (char*)malloc(20);
-			strcpy (answer, "Incorrect request\n");
-		}
-		//writeClient (ms, (const char*)answer);
-		str_length = strlen(answer);
-		//write(ms, answer, str_length);
-		puts (answer);
-		free (answer);
-		//close (ms);
-		answer = NULL;
+   
+    while (1) {
+        
+        read_set = active_set;
+        if ( select(FD_SETSIZE,&read_set,NULL,NULL,NULL)<0 ) {
+            perror("Server: select  failure");
+            exit (EXIT_FAILURE);
+        }
+
+        
+        for (i=0; i<FD_SETSIZE; i++) {
+            if ( FD_ISSET(i,&read_set) ) {
+                if ( i==sock ) {
+                    
+                    size = sizeof(client);
+                    new_sock = accept(sock,(struct sockaddr*)&client,&size);
+                    if ( new_sock<0 ) {
+                        perror("accept");
+                        exit (EXIT_FAILURE);
+                    }
+                    fprintf (stdout, "Server: connect from host %s, port %hu.\n",
+                            inet_ntoa(client.sin_addr),
+                            ntohs(client.sin_port));
+                    FD_SET(new_sock, &active_set);
+                } else {
+                    
+                    err = readFromClient(i,buf);
+                    if ( err<0 ) {
+                        
+                        close (i);
+                        FD_CLR(i,&active_set);
+                    } else {
+                        
+                        if ( strstr(buf,"stop") ) {
+                            close(i);
+                            FD_CLR (i,&active_set);
+                        } else {
+							if (strcmp(buf, "quit") == 0 ) {
+								answer = (char*)malloc(7);
+								strcpy (answer, "Quit\n");
+								puts (answer);
+								free (answer);
+								in = fopen ("text.txt","w");
+								for (k = 0; k < Par_Num; k++) {
+									fprintf (in, "%s\n", text[k]);
+									free(text[k]);
+								}
+								free(text);
+								fclose(in);
+								return 0;
+							}
+							if (strncmp(buf, "show", 4) == 0) answer = show(text, Par_Num);
+							if (strncmp(buf, "add", 3) == 0) {
+								k = 3;
+								while (buf[k] < '0'&&buf[k]>'9') k++;
+								chislo = atoi(&buf[i]);
+								k = k + kek(chislo)+1;
+								while (buf[k] == ' ') k++;
+								if (chislo < 0||chislo > Par_Num+1) {
+									answer = (char*)malloc(22);
+									strcpy (answer, "Incorrect number\n");
+								}
+								else {
+									text = (char**)realloc(text, (Par_Num+1)*sizeof(char*));
+									text[Par_Num] = (char*)malloc(1026);
+									answer = add (text, Par_Num, &buf[k], chislo);
+									Par_Num++;
+								}
+							}
+							if (strncmp(buf, "search", 6) == 0) {
+								k = 6;
+								while (buf[k] == ' ') k++;
+								chislo = atoi(&buf[k]);
+								k = k + kek(chislo) + 1;
+								while (buf[k] == ' ') k++;
+								answer = search (text, Par_Num, &buf[k], chislo);
+							}
+							if (strncmp(buf, "delete", 6) == 0) {
+								k = 6;
+								while (buf[k] < '0'&&buf[k]>'9') k++;
+								chislo = atoi(&buf[k]);
+								if (chislo < 0||chislo > Par_Num) {
+									answer = (char*)malloc(22);
+									strcpy (answer, "Incorrect number\n");
+								}
+								else {
+									answer = del (text, Par_Num, chislo);
+									free (text[Par_Num-1]);
+									text = (char**)realloc(text, (Par_Num-1)*sizeof(char*));
+									Par_Num--;
+								}
+							}
+							if (answer == NULL) {
+								answer = (char*)malloc(20);
+								strcpy (answer, "Incorrect request\n");
+							}
+							
+                            writeToClient(i,answer);
+							free (answer);
+							answer = NULL;
+                        }
+                    }
+                }
+            }
+        }
     }
-	// djldkjs
-	in = fopen ("text.txt","w");
-	for (i = 0; i < Par_Num; i++) {
-		fprintf (in, "%s\n", text[i]);
-		free(text[i]);
-	}
-	free(text);
-	fclose(in);
-    close(as);
-    return 0;
+	return 0;
 }
+
+
+
+int  readFromClient (int fd, char *buf)
+{
+    int  nbytes;
+
+    nbytes = read(fd,buf,BUFLEN);
+    if ( nbytes<0 ) {
+        // РѕС€РёР±РєР° С‡С‚РµРЅРёСЏ
+        perror ("Server: read failure");
+        return -1;
+    } else if ( nbytes==0 ) {
+        // Р±РѕР»СЊС€Рµ РЅРµС‚ РґР°РЅРЅС‹С…
+        return -1;
+    } else {
+        // РµСЃС‚СЊ РґР°РЅРЅС‹Рµ
+        fprintf(stdout,"Server got message: %s\n",buf);
+        return 0;
+    }
+}
+
+
+
+void  writeToClient (int fd, char *buf)
+{
+    int  nbytes;
+    unsigned char *s;
+	nbytes = strlen(buf);
+    //for (s=(unsigned char*)buf; *s; s++) *s = toupper(*s);
+	write (fd, &nbytes, 4);
+    nbytes = write(fd,buf,strlen(buf)+1);
+    fprintf(stdout,"Write back: %s\nnbytes=%d\n",buf,nbytes);
+    
+    if ( nbytes<0 ) {
+        perror ("Server: write failure");
+    }
+}
+
