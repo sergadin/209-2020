39523ade2fef4d124c56c4b8d27cc9157fe1487d
Sergey Afonin <serg@msu.ru>
2020-06-05 20:45:35 +0300

    Fix some syntax errors

--- AleksandraNikolenko/sea_battle/server.h
+++ AleksandraNikolenko/sea_battle/server.h
@@ -1,3 +1,4 @@
+#include <sstream>
 #include <iostream>
 #include <map>
 #include<math.h>
@@ -14,6 +15,8 @@ enum {OK, WS, OpM, SM ,SO, Q, NC, LOSE, READY} RES; //ws - введите кор
 
 using namespace std;
 
+class Player;
+class Game;
 
 class Ship {
 	private:
@@ -74,39 +77,6 @@ class Ship {
 		friend class Player;
 };
 
-class Game {
-	private:
-		Player* pl_1;
-		Player* pl_2;
-	public:
-		Game(Player &pl1, Player &pl2)
-		{
-			pl_1 = pl1;
-			pl_2 = pl2;
-			pl_1->game_ = this;
-			pl_2->game_ = this;
-			pl_1->status_ = InGame;
-			pl_2->status_ = InGame;
-			pl_1->numb_ = 1;
-			pl_2->numb_ = 2;
-		}
-		void change_q()
-		{
-			this->pl_1->queue_ = !(pl_1->queue_);
-			this->pl_2->queue_ = !(pl_2->queue_);
-		}
-		int who()
-		{
-			if(this->pl_1->queue_)
-				return(pl_1->sock_);
-			if(this->pl_2->queue_)
-				return(pl_2->sock_);
-		}
-		
-		friend class Player;
-};
-
-
 class Player {
 	private:
 		Ship* ships_[10] = {NULL};
@@ -125,126 +95,7 @@ class Player {
 			status_ = FREE;
 			queue_ = true;
 		}
-			
-		int analisys(char* buf, map<int, Player*> &Players)
-		{
-			Player *pl_2;
-			string query;
-			int row, col;
-			string ori;
-			string str = buf;
-			if(strcmp(buf, "Quit") == 0)
-				return Q;
-			else if(strcmp(buf, "ShowMy") == 0)
-				return SM;
-			else if(strcmp(buf, "ShowOther") == 0)
-				return SO;
-			else if(strstr(buf, "Ships") != NULL)
-			{
-				stringstream ss(str);
-				getline(ss, query, ':');
-				if(strcmp(&query, "Ships") != 0)
-						return -1;
-				else
-				{
-					for(int i = 0; i < 10; i++)
-					{
-						ss >> row >> col >> ori;
-						ss.ignore();
-						if(0 <= i < 4)
-							ships_[i] = new Ship(row, col, ori, 1);
-						else if(4 <= i < 7)
-							ships_[i] = new Ship(row, col, ori, 2);
-						else if(7 <= i < 9)
-							ships_[i] = new Ship(row, col, ori, 3);
-						else
-							ships_[i] = new Ship(row, col, ori, 4);
-					}
-					qs = 10;
-					this->game_->change_q();
-					return OK;
-				}
-			}
-			if(!(this->queue))
-				return OpM;
-			else{
-				if(strcmp(buf, "Play") == 0)
-				{
-					pl_2 = search(map<int, Player*> &Players);  //если соперник найден - введите позиции кораблей
-					if(pl_2 == NULL)
-						return -1;
-					else
-					{
-						game_ = new Game(this, pl_2);
-						this->queue_ = true;
-						pl_2->queue_ = false;
-						return WS;
-					}
-				}
-				
-				else if(strstr(buf, "Move") != NULL)
-				{
-					stringstream ss(str);
-					getline(ss, query, ':');
-					if(strcmp(query, "Move") != 0)
-							return NC;
-					else
-					{
-						ss >> row >> col;
-						if(this->numb_ = 1)
-						{
-							if(this->game->pl_2->hit(row, col) == miss)
-							{
-								this->game->change_q();
-								this->my_moves.push_back(row*10 + col);
-								if(this->game->pl_2->lose())
-									return LOSE;
-								return OK;
-							}
-							
-						}
-						if(this->numb_ = 2)
-						{
-							if(this->game->pl_1->hit(row, col) == miss)
-							{
-								this->game->change_q();
-								this->my_moves.push_back(row*10 + col);
-								if(this->game->pl_1->lose())
-									return LOSE;
-								return OK;
-							}
-						}
-					}
-				}
-				else if(strstr(buf, "Ships") != NULL)
-				{
-					stringstream ss(str);
-					getline(ss, query, ':');
-					if(strcmp(query, "Ships") != 0)
-							return -1;
-					else
-					{
-						for(int i = 0; i < 10, i++)
-						{
-							ss >> row >> col >> ori;
-							ss.ignore();
-							if(0 <= i < 4)
-								ships_[i] = new Ship(row, col, ori, 1);
-							else if(4 <= i < 7)
-								ships_[i] = new Ship(row, col, ori, 2);
-							else if(7 <= i < 9)
-								ships_[i] = new Ship(row, col, ori, 3);
-							else
-								ships_[i] = new Ship(row, col, ori, 4);
-						}
-						qs_ = 10;
-						this->game_->change_q();
-						return OK;
-					}
-				}
-			}
-			return -1;
-		}
+        int analisys(char* buf, map<int, Player*> &Players);
 		vector<int>* get_moves()
 		{
 			return &my_moves;
@@ -260,8 +111,8 @@ class Player {
 			map <int, Player*> :: iterator it = Players->begin();
 			while(it != Players->end())
 			{
-				if(Players[it]->status_ == FREE)
-					return map[it];
+				if(it->second->status_ == FREE)
+					return it->second;
 				else it++;
 			}
 			if(it == Players->end())
@@ -297,6 +148,37 @@ class Player {
 	friend class Game;
 };
 
+class Game {
+	private:
+		Player* pl_1;
+		Player* pl_2;
+	public:
+		Game(Player *pl1, Player *pl2)
+		{
+			pl_1 = pl1;
+			pl_2 = pl2;
+			pl_1->game_ = this;
+			pl_2->game_ = this;
+			pl_1->status_ = InGame;
+			pl_2->status_ = InGame;
+			pl_1->numb_ = 1;
+			pl_2->numb_ = 2;
+		}
+		void change_q()
+		{
+			this->pl_1->queue_ = !(pl_1->queue_);
+			this->pl_2->queue_ = !(pl_2->queue_);
+		}
+		int who()
+		{
+			if(this->pl_1->queue_)
+				return(pl_1->sock_);
+			if(this->pl_2->queue_)
+				return(pl_2->sock_);
+		}
+		
+		friend class Player;
+};
 
 
 
@@ -327,4 +209,126 @@ class Player {
 
 
 
+
+
+
+int Player::analisys(char* buf, map<int, Player*> &Players)
+{
+    Player *pl_2;
+    string query;
+    int row, col;
+    string ori;
+    string str = buf;
+    if(strcmp(buf, "Quit") == 0)
+        return Q;
+    else if(strcmp(buf, "ShowMy") == 0)
+        return SM;
+    else if(strcmp(buf, "ShowOther") == 0)
+        return SO;
+    else if(strstr(buf, "Ships") != NULL)
+    {
+        stringstream ss(str);
+        getline(ss, query, ':');
+        if(strcmp(query.c_str(), "Ships") != 0)
+            return -1;
+        else
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                ss >> row >> col >> ori;
+                ss.ignore();
+                if(0 <= i < 4)
+                    ships_[i] = new Ship(row, col, ori, 1);
+                else if(4 <= i < 7)
+                    ships_[i] = new Ship(row, col, ori, 2);
+                else if(7 <= i < 9)
+                    ships_[i] = new Ship(row, col, ori, 3);
+                else
+                    ships_[i] = new Ship(row, col, ori, 4);
+            }
+            qs = 10;
+            this->game_->change_q();
+            return OK;
+        }
+    }
+    if(!(this->queue))
+        return OpM;
+    else{
+        if(strcmp(buf, "Play") == 0)
+        {
+            pl_2 = search(map<int, Player*> &Players);  //если соперник найден - введите позиции кораблей
+            if(pl_2 == NULL)
+                return -1;
+            else
+            {
+                game_ = new Game(this, pl_2);
+                this->queue_ = true;
+                pl_2->queue_ = false;
+                return WS;
+            }
+        }
+
+        else if(strstr(buf, "Move") != NULL)
+        {
+            stringstream ss(str);
+            getline(ss, query, ':');
+            if(strcmp(query, "Move") != 0)
+                return NC;
+            else
+            {
+                ss >> row >> col;
+                if(this->numb_ = 1)
+                {
+                    if(this->game->pl_2->hit(row, col) == miss)
+                    {
+                        this->game->change_q();
+                        this->my_moves.push_back(row*10 + col);
+                        if(this->game->pl_2->lose())
+                            return LOSE;
+                        return OK;
+                    }
+
+                }
+                if(this->numb_ = 2)
+                {
+                    if(this->game->pl_1->hit(row, col) == miss)
+                    {
+                        this->game->change_q();
+                        this->my_moves.push_back(row*10 + col);
+                        if(this->game->pl_1->lose())
+                            return LOSE;
+                        return OK;
+                    }
+                }
+            }
+        }
+        else if(strstr(buf, "Ships") != NULL)
+        {
+            stringstream ss(str);
+            getline(ss, query, ':');
+            if(strcmp(query, "Ships") != 0)
+                return -1;
+            else
+            {
+                for(int i = 0; i < 10, i++)
+                {
+                    ss >> row >> col >> ori;
+                    ss.ignore();
+                    if(0 <= i < 4)
+                        ships_[i] = new Ship(row, col, ori, 1);
+                    else if(4 <= i < 7)
+                        ships_[i] = new Ship(row, col, ori, 2);
+                    else if(7 <= i < 9)
+                        ships_[i] = new Ship(row, col, ori, 3);
+                    else
+                        ships_[i] = new Ship(row, col, ori, 4);
+                }
+                qs_ = 10;
+                this->game_->change_q();
+                return OK;
+            }
+        }
+    }
+    return -1;
+}
 
