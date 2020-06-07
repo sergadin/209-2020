96f7499aada23cbc0c2dc2f7d74ec3442b64c270
SivikGod <59839973+SivikGod@users.noreply.github.com>
2020-06-07 17:13:07 +0300

    Update client.py

--- SivolapNN/client.py
+++ SivolapNN/client.py
@@ -1,23 +1,25 @@
 import socket
+import struct
 
 stop_word = '__4to-to__'
-
-def chunkstring(string, length):
-    return (string[0+i:length+i] for i in range(0, len(string), length))
 
 inp = input('Enter server adress and port separated by comma\n')
 addr, port = inp.split(',')
 
-sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((addr.strip(), int(port)))
+sock.recv(1, socket.MSG_WAITALL)
 print('Ready')
 while True:
     inp = input()
     if inp.startswith('delete') and not inp.endswith('end'):
         print('Delete request should ends with \'end\' word')
         continue
-    for substr in chunkstring(inp+' ', 256):
-        sock.send(substr.encode())
+    sock.send(struct.pack('!I', len(inp)))
+    sended = 0
+    while sended < len(inp):
+        sended += sock.send(inp[sended:].encode())
+        
     if inp in ['disconnect', 'SHUTDOWN']:
         sock.close()
         break
@@ -28,4 +30,5 @@ while True:
         if data.endswith(stop_word):
             break
     print(data[:-len(stop_word)])
+
 
