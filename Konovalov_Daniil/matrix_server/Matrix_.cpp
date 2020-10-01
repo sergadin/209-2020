665b434002cf70f7bb73259c53d31cba6e190c34
Hedgehoged <48029797+Hedgehoged@users.noreply.github.com>
2020-10-01 13:06:50 +0300

    Update Matrix_.cpp

--- Konovalov_Daniil/matrix_server/Matrix_.cpp
+++ Konovalov_Daniil/matrix_server/Matrix_.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string>
 
 #include "Matrix_.h"
 
@@ -9,15 +10,15 @@ Matrix::Matrix(int n, int m)
 	this->n_ = n;
 	this->m_ = m;
 	
-	this->mat = new double[n];
+	this->mat_ = new double*[n];
 	
 	for(int i=0;i<n;i++)
 	{
-		this->mat[i] = new double[m];
+		this->mat_[i] = new double[m];
 		
 		for(int j=0;j<m;j++)
 		{
-			this->mat[i][j] = 0;
+			this->mat_[i][j] = 0;
 		}
 	}
 }
@@ -39,8 +40,9 @@ Matrix::Matrix(char* stroka)
 	char* cmd = (char*)malloc(i);
 	
 	int k=i;
-	
-	while (int j=0; j<i; j++)
+	int j=0;
+
+	for(j=0; j < i; j++)
 	{
 		cmd[j] = stroka[j];
 	}
@@ -49,8 +51,7 @@ Matrix::Matrix(char* stroka)
 	free(cmd);
 	if(n<0)
 	{
-		printf("\nWrong rows count brUh\n");
-		return NULL;
+		perror("\nWrong rows count brUh\n");
 	}
 	
 	while((stroka[i] != ' ') || (stroka[i] != '\n') || (stroka[i] != '\0'))
@@ -60,7 +61,7 @@ Matrix::Matrix(char* stroka)
 	
 	char* comd = (char*)malloc(i-k);
 	
-	while (int j=0; j<i-k; j++)
+	for (int j=0; j<i-k; j++)
 	{
 		comd[j] = stroka[j];
 	}
@@ -70,17 +71,16 @@ Matrix::Matrix(char* stroka)
 	
 	if(m<0)
 	{
-		printf("\nWrong cols count bruH\n");
-		return NULL;
+		perror("\nWrong cols count bruH\n");
 	}
 	
-	double* massiv = (double)malloc(n*m);
+	double* massiv = new double[n*m];
 	int p = 0;
 		
 		while(i < sizeof(stroka))
 		{	
 			int k = i;
-			dlina = 0;
+			int dlina = 0;
 			
 			while(stroka[k] != ' ')
 			{
@@ -90,14 +90,14 @@ Matrix::Matrix(char* stroka)
 			
 			k=i;
 			
-			char* suda =(char)malloc(dlina);
+			char* suda = new char[dlina];
 			int m = 0;
 			
 			while(m < dlina)
 			{
 				suda[m] = stroka[k];
 				m++;
-				k++
+				k++;
 			}
 			
 			massiv[p] = atoi(suda);
@@ -109,16 +109,19 @@ Matrix::Matrix(char* stroka)
 	
 	this->n_ = n;
 	this->m_ = m;
-	this->mat_ = new double[n][m];
+	this->mat_ = new double*[n];
+
+	for(int q=0; q<n; q++)
+	{
+		this->mat_[q] = new double[m];
+	}
 	
 	oneD_to_twoD(massiv, this->mat_, n_ , m_);
 	
 	free(massiv);
-	
-	return matrica;
 }
 
-void Matrix::oneD_to_twoD(double* mat1, double** mat2, int n, int m)
+double** oneD_to_twoD(double* mat1, double** mat2, int n, int m)
 {
 	for(int i=0;i<n;i++)
 	{
@@ -127,9 +130,11 @@ void Matrix::oneD_to_twoD(double* mat1, double** mat2, int n, int m)
 			mat2[i][j] = mat1[i*j +j];
 		}
 	}
+
+	return mat2;
 }
 
-void Matrix::twoD_to_oneD(double** mat1, int n, int m, double mat2)
+double* twoD_to_oneD(double** mat1, int n, int m, double* mat2)
 {
 	for(int i=0;i<n;i++)
 	{
@@ -138,21 +143,23 @@ void Matrix::twoD_to_oneD(double** mat1, int n, int m, double mat2)
 			mat2[i*j +j] = mat1[i][j];
 		}
 	}
+
+	return mat2;
 }
 
 Matrix::Matrix(double** mat, int n, int m)
-{
-	Matrix matr(n, m);
-	
+{	
+	this->n_ = n;
+	this->m_ = m;
+
 	for(int i=0; i<n; i++)
 	{
 		for(int j=0; j<m; j++)
 		{
-			matr[i][j] = mat[i][j];
+			this->mat_[i][j] = mat[i][j];
 		}
 	}
 	
-	return matr;
 }
 
 double Matrix::get_mat_elem(int i, int j)
@@ -162,10 +169,10 @@ double Matrix::get_mat_elem(int i, int j)
 
 double** Matrix::get_mat_data()
 {
-	return this->data_;
+	return this->mat_;
 }
 
-Matrix::Matrix mat_mult(Matrix& mat1, Matrix& mat2)
+Matrix* mat_mult(Matrix& mat1, Matrix& mat2)
 {
 
 	if(mat1.get_mat_col() != mat2.get_mat_row()) {
@@ -177,25 +184,26 @@ Matrix::Matrix mat_mult(Matrix& mat1, Matrix& mat2)
 	int new_m = mat2.get_mat_col();
 	int mid_size = mat1.get_mat_col();
 
-	int** new_Mat = new int*[new_n];
+	double** new_Mat = new double*[new_n];
 	for(int i = 0; i < new_n; i++) 
 	{
-		new_Mat[i] = new int[new_m];
+		new_Mat[i] = new double[new_m];
 
 		for(int j = 0; j < new_m; j++) 
 		{
 			new_Mat[i][j] = 0;
 			for(int k = 0; k < mid_size; k++) 
 			{
-				new_Mat[i][j] += mat1[i][k]*mat2[k][j];
+				new_Mat[i][j] += mat1.get_mat_elem(i,k)*mat2.get_mat_elem(k,j);
 			}
 		}
 	}
-
-	return Matrix(new_n, new_m, new_Mat);
+	Matrix* matrichka = new Matrix(new_Mat, new_n, new_m);
+	
+	return matrichka;
 }
 
-bool Matrix::can_i_multiply(Matrix& matr1, Matrix& matr2)
+bool can_i_multiply(Matrix& matr1, Matrix& matr2)
 {
 	return (matr1.get_mat_col() == matr2.get_mat_row());
 }
@@ -203,15 +211,27 @@ bool Matrix::can_i_multiply(Matrix& matr1, Matrix& matr2)
 char* matrix_to_string(Matrix& matrix)
 {
 	double* massiv = new double[matrix.get_mat_row()*matrix.get_mat_col()];
-	twoD_to_oneD(matrix.get_mat_data(), matrix.get_mat_row(), matrix.get_mat_col(), massiv);
-	char* massivchik = new char[sizeof(massiv)*3];
+	massiv = twoD_to_oneD(matrix.get_mat_data(), matrix.get_mat_row(), matrix.get_mat_col(), massiv);
+	char** massivchik = new char*[sizeof(massiv)];
+
+
 	
-	for(int i=0; i<sizeof(massiv)*3; i++)
+	for(int i=0; i<sizeof(massiv); i++)
 	{
-		massivchik[i] = to_string(massiv[i]);
+		massivchik[i] = new char[3];
+		sprintf(massivchik[i], "%f", massiv[i]);
+	}
+
+	char* mat_string = new char[sizeof(massiv)*3];
+
+	for(int i=0; i<sizeof(massiv); i++)
+	{
+		strcat(mat_string, massivchik[i]);
+		free(massivchik[i]);
 	}
 	
 	free(massiv);
-	
-	return massivchik;
+	free(massivchik);
+
+	return mat_string;
 }
